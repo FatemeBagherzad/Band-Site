6ace0dee5451b78ca013a2e7f79e4242b78b7b84
Fateme <fa.bagherzad@gmail.com>
2023-06-25 21:49:03 -0400

    fix path of fonts and images

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -2,30 +2,30 @@ const commentArr = [
   {
     userName: "Connor Walton",
     commentDate: "02/17/2021",
-    imgSrc: "../assets/images/person4.jpg",
+    imgSrc: "./assets/images/person4.jpg",
     userComment:
       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it  contains.",
   },
   {
     userName: "Emilie Beach",
     commentDate: "01/09/2021",
-    imgSrc: "../assets/images/person3.jpg",
+    imgSrc: "./assets/images/person3.jpg",
     userComment:
       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one nay of my life I could relive, this would be it. What an incredible day.",
   },
   {
     userName: "Miles Acosta",
     commentDate: "12/20/2020",
-    imgSrc: "../assets/images/person5.jpg",
+    imgSrc: "./assets/images/person5.jpg",
     userComment:
       "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
   },
 ];
 
 const commentArrImg = [
-  "../assets/images/Mohan-muruge.jpg",
-  "../assets/images/person2.jpg",
-  "../assets/images/person4.jpg",
+  "./assets/images/Mohan-muruge.jpg",
+  "./assets/images/person2.jpg",
+  "./assets/images/person4.jpg",
 ];
 
 /////////////////////////////////////////////////
