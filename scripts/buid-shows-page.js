7c38fc1a6769f7782e78c0595bd0b9143cda85d4
Fateme <fa.bagherzad@gmail.com>
2023-07-15 23:54:14 -0400

    Use axion and get API key and get 3 default comments

--- scripts/buid-shows-page.js
+++ scripts/buid-shows-page.js
@@ -1,33 +1,33 @@
 let showsInfo = [
   {
-    date: "Mon Sept 06 2021",
-    venu: "Ronald Lane",
-    location: "SanFrancisco, CA",
+    date: 'Mon Sept 06 2021',
+    venu: 'Ronald Lane',
+    location: 'SanFrancisco, CA',
   },
   {
-    date: "Tue Sept 21 2021",
-    venu: "Pier 3 East",
-    location: "SanFrancisco, CA",
+    date: 'Tue Sept 21 2021',
+    venu: 'Pier 3 East',
+    location: 'SanFrancisco, CA',
   },
   {
-    date: "Fri Oct 15 2021",
-    venu: "View Lounge",
-    location: "SanFrancisco, CA",
+    date: 'Fri Oct 15 2021',
+    venu: 'View Lounge',
+    location: 'SanFrancisco, CA',
   },
   {
-    date: "Sat Nov 06 2021",
-    venu: "Hyatt Agency",
-    location: "SanFrancisco, CA",
+    date: 'Sat Nov 06 2021',
+    venu: 'Hyatt Agency',
+    location: 'SanFrancisco, CA',
   },
   {
-    date: "Fri Nov 26 2021",
-    venu: "Moscow Center",
-    location: "SanFrancisco, CA",
+    date: 'Fri Nov 26 2021',
+    venu: 'Moscow Center',
+    location: 'SanFrancisco, CA',
   },
   {
-    date: "Wed Dec 15 2021",
-    venu: "Press Club",
-    location: "SanFrancisco, CA",
+    date: 'Wed Dec 15 2021',
+    venu: 'Press Club',
+    location: 'SanFrancisco, CA',
   },
 ];
 
@@ -41,38 +41,38 @@ let createElement = function (elem, classArr) {
 };
 
 //creating top parents
-let shows = document.querySelector(".shows");
-let header = document.querySelector(".shows__header");
-let tableWrapper = document.querySelector(".shows__tableWrapper");
+let shows = document.querySelector('.shows');
+let header = document.querySelector('.shows__header');
+let tableWrapper = document.querySelector('.shows__tableWrapper');
 
 //-----------------------------------
 //mobile
 //-----------------------------------
-let tableMobile = createElement("table", ["shows__table"]);
-tableMobile.setAttribute("id", "mobileTable");
+let tableMobile = createElement('table', ['shows__table']);
+tableMobile.setAttribute('id', 'mobileTable');
 
 tableWrapper.appendChild(tableMobile);
 
 let concertInfo = function (arr) {
   for (i = 0; i < arr.length; i++) {
-    let dateTitle = createElement("div", ["shows__table--title"]);
-    dateTitle.innerHTML = "DATE";
-    let dateM = createElement("div", [
-      "shows__table--item",
-      "shows__table--item-boldItem",
+    let dateTitle = createElement('div', ['shows__table--title']);
+    dateTitle.innerHTML = 'DATE';
+    let dateM = createElement('div', [
+      'shows__table--item',
+      'shows__table--item-boldItem',
     ]);
     dateM.innerHTML = arr[i].date;
-    let venuTitle = createElement("div", ["shows__table--title"]);
-    venuTitle.innerHTML = "VENU";
-    let venuM = createElement("div", ["shows__table--item"]);
+    let venuTitle = createElement('div', ['shows__table--title']);
+    venuTitle.innerHTML = 'VENU';
+    let venuM = createElement('div', ['shows__table--item']);
     venuM.innerHTML = arr[i].venu;
-    let locationTitle = createElement("div", ["shows__table--title"]);
-    locationTitle.innerHTML = "LOCATION";
-    let locationM = createElement("div", ["shows__table--item"]);
+    let locationTitle = createElement('div', ['shows__table--title']);
+    locationTitle.innerHTML = 'LOCATION';
+    let locationM = createElement('div', ['shows__table--item']);
     locationM.innerHTML = arr[i].location;
-    let ticketBtn = createElement("button", ["shows__table--btn"]);
-    ticketBtn.innerHTML = "BUY TICKET";
-    let hr = createElement("hr", ["shows__table--hr"]);
+    let ticketBtn = createElement('button', ['shows__table--btn']);
+    ticketBtn.innerHTML = 'BUY TICKET';
+    let hr = createElement('hr', ['shows__table--hr']);
 
     tableMobile.appendChild(dateTitle);
     tableMobile.appendChild(dateM);
@@ -90,22 +90,22 @@ concertInfo(showsInfo);
 //-----------------------------------
 //Desktop and Tablet
 //-----------------------------------
-let desktopTable = createElement("section", ["shows__table"]);
-desktopTable.setAttribute("id", "tabletDesktopTable");
+let desktopTable = createElement('section', ['shows__table']);
+desktopTable.setAttribute('id', 'tabletDesktopTable');
 
 tableWrapper.appendChild(desktopTable);
 
-let titleWrapperDesktop = createElement("div", ["tableLayout"]);
+let titleWrapperDesktop = createElement('div', ['tableLayout']);
 
-let dateTitle = createElement("div", ["shows__table--title"]);
-dateTitle.innerHTML = "DATE";
-let venuTitle = createElement("div", ["shows__table--title"]);
-venuTitle.innerHTML = "VENU";
-let locationTitle = createElement("div", ["shows__table--title"]);
-locationTitle.innerHTML = "LOCATION";
-let emptyTitle = createElement("div", [
-  "shows__table--title",
-  "tabletitlehidden",
+let dateTitle = createElement('div', ['shows__table--title']);
+dateTitle.innerHTML = 'DATE';
+let venuTitle = createElement('div', ['shows__table--title']);
+venuTitle.innerHTML = 'VENU';
+let locationTitle = createElement('div', ['shows__table--title']);
+locationTitle.innerHTML = 'LOCATION';
+let emptyTitle = createElement('div', [
+  'shows__table--title',
+  'tabletitlehidden',
 ]);
 
 titleWrapperDesktop.appendChild(dateTitle);
@@ -114,27 +114,27 @@ titleWrapperDesktop.appendChild(locationTitle);
 titleWrapperDesktop.appendChild(emptyTitle);
 desktopTable.appendChild(titleWrapperDesktop);
 
-let concertInfoWrapper = createElement("div", [
-  "shows__table--border",
-  "tableLayout",
+let concertInfoWrapper = createElement('div', [
+  'shows__table--border',
+  'tableLayout',
 ]);
 
 showsInfo.forEach(function (show) {
-  let row = createElement("div", ["tableLayout", "shows__table--border"]);
-  let concertDate = createElement("div", [
-    "shows__table--item",
-    "shows__table--item-boldItem",
+  let row = createElement('div', ['tableLayout', 'shows__table--border']);
+  let concertDate = createElement('div', [
+    'shows__table--item',
+    'shows__table--item-boldItem',
   ]);
   concertDate.innerHTML = show.date;
-  let concertVenu = createElement("div", ["shows__table--item"]);
+  let concertVenu = createElement('div', ['shows__table--item']);
   concertVenu.innerHTML = show.venu;
-  let concertLocation = createElement("div", ["shows__table--item"]);
+  let concertLocation = createElement('div', ['shows__table--item']);
   concertLocation.innerHTML = show.location;
-  let divBtn = createElement("div", []);
-  let concertTicket = createElement("button", ["shows__table--btn"]);
-  concertTicket.textContent = "BUY TICKET";
+  let divBtn = createElement('div', []);
+  let concertTicket = createElement('button', ['shows__table--btn']);
+  concertTicket.textContent = 'BUY TICKET';
   divBtn.appendChild(concertTicket);
-  let hr = createElement("hr", ["shows__table--hr"]);
+  let hr = createElement('hr', ['shows__table--hr']);
 
   row.appendChild(concertDate);
   row.appendChild(concertVenu);
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,67 +1,66 @@
-const commentArr = [
-  {
-    userName: "Connor Walton",
-    commentDate: "02/17/2021",
-    imgSrc: "./assets/images/person4.jpg",
-    userComment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it  contains.",
-  },
-  {
-    userName: "Emilie Beach",
-    commentDate: "01/09/2021",
-    imgSrc: "./assets/images/person3.jpg",
-    userComment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one nay of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    userName: "Miles Acosta",
-    commentDate: "12/20/2020",
-    imgSrc: "./assets/images/person5.jpg",
-    userComment:
-      "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
-  },
-];
+let commentArr = [];
+const APIKey = axios
+  .get('https://project-1-api.herokuapp.com/register')
+  .then((result) => {
+    axios
+      .get(
+        `https://project-1-api.herokuapp.com/comments?api_key=${result.data.api_key}`
+      )
+      .then((result1) => {
+        commentArr.push(...result1.data);
+        console.log(commentArr); //test
+        for (i = 0; i < commentArr.length; i++) {
+          displayComment(commentArr[i]);
+        }
+      });
+  });
+
+//----------------------------------
 
 const commentArrImg = [
-  "./assets/images/Mohan-muruge.jpg",
-  "./assets/images/person2.jpg",
-  "./assets/images/person4.jpg",
+  './assets/images/person1.jpg',
+  './assets/images/person2.jpg',
+  './assets/images/person3.jpg',
+  './assets/images/person4.jpg',
+  './assets/images/person5.jpg',
 ];
 
 /////////////////////////////////////////////////
 
-const form = document.querySelector(".formSection");
-let commentSection = document.querySelector(".commentSection");
+const form = document.querySelector('.formSection');
+let commentSection = document.querySelector('.commentSection');
 
-form.addEventListener("submit", function (event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
   let userN = event.target.name.value;
 
   //define current Date
-  let day = new Date().getDate();
-  let month = new Date().getMonth() + 1;
-  let year = new Date().getFullYear();
-  let dateVal = `${day}/${month}/${year}`;
+  let day = Math.round(Date.now());
+
+  console.log(day);
+  // let month = new Date().getMonth() + 1;
+  // let year = new Date().getFullYear();
+  // let dateVal = `${day}/${month}/${year}`;
 
   let commentVal = event.target.comment.value;
 
   //removing all previous comments
-  let element = document.getElementById("commentSection");
+  let element = document.getElementById('commentSection');
 
   //Alert for incompelete form
   if (!userN || !commentVal) {
     alert(`📣Please fill out all fields! `);
-    document.querySelector(".formSection__form--input").style.borderColor =
-      "red";
+    document.querySelector('.formSection__form--input').style.borderColor =
+      'red';
     return;
   } else {
-    element.innerText = "";
-    document.querySelector(".formSection__form--input").style.borderColor =
-      " #e1e1e1";
+    element.innerText = '';
+    document.querySelector('.formSection__form--input').style.borderColor =
+      ' #e1e1e1';
     const newComment = {
-      userName: userN,
-      userComment: commentVal,
-      commentDate: dateVal,
+      name: userN,
+      comment: commentVal,
+      timestamp: day,
     };
     //put new input in array and pop one fron end of
     commentArr.unshift(newComment);
@@ -71,8 +70,8 @@ form.addEventListener("submit", function (event) {
       commentArr[i].imgSrc = commentArrImg[i];
       displayComment(commentArr[i]);
     }
-    document.getElementById("name").value = "";
-    document.getElementById("comment").value = "";
+    document.getElementById('name').value = '';
+    document.getElementById('comment').value = '';
   }
 });
 
@@ -80,40 +79,47 @@ form.addEventListener("submit", function (event) {
 //Function that take an object as parameter and make a comment section
 function displayComment(obj) {
   //creating main section
-  let comment = document.createElement("div");
-  comment.classList.add("comment");
+  let comment = document.createElement('div');
+  comment.classList.add('comment');
 
   //adding image
-  let image = document.createElement("img");
-  image.classList.add("comment__img");
-  image.src = obj.imgSrc;
+  let image = document.createElement('img');
+  image.classList.add('comment__img');
+  image.src = commentArrImg[Math.trunc(Math.random() * 3)];
 
   //create div wrapper of name and date and p
-  let commentTxtWrapper = document.createElement("div");
-  commentTxtWrapper.classList.add("comment__TxtWrapper");
+  let commentTxtWrapper = document.createElement('div');
+  commentTxtWrapper.classList.add('comment__TxtWrapper');
 
   //create div wrapper of Name and date
-  let userInfo = document.createElement("div");
-  userInfo.classList.add("comment__TxtWrapper--userInfo");
+  let userInfo = document.createElement('div');
+  userInfo.classList.add('comment__TxtWrapper--userInfo');
 
   //create container and add username
-  let userName = document.createElement("span");
-  userName.classList.add("comment__TxtWrapper--userInfo-userN");
-  userName.innerHTML = obj.userName;
+  let userName = document.createElement('span');
+  userName.classList.add('comment__TxtWrapper--userInfo-userN');
+  userName.innerHTML = obj.name;
 
   //create container and add date
-  let date = document.createElement("span");
-  date.classList.add("comment__TxtWrapper--userInfo-date");
-  date.innerHTML = obj.commentDate;
+  let date = document.createElement('span');
+  date.classList.add('comment__TxtWrapper--userInfo-date');
+  let dateFormat = new Date(obj.timestamp);
+  let dateFinalFormat =
+    dateFormat.getDate() +
+    '/' +
+    (dateFormat.getMonth() + 1) +
+    '/' +
+    dateFormat.getFullYear();
+  date.innerHTML = dateFinalFormat;
 
   //create container and add comment
-  let usercomment = document.createElement("p");
-  usercomment.classList.add("comment__TxtWrapper--p");
-  usercomment.innerHTML = obj.userComment;
+  let usercomment = document.createElement('p');
+  usercomment.classList.add('comment__TxtWrapper--p');
+  usercomment.innerHTML = obj.comment;
 
   //create line between comments
-  let hr = document.createElement("hr");
-  hr.classList.add("comment__hr");
+  let hr = document.createElement('hr');
+  hr.classList.add('comment__hr');
 
   //create whole comment section by adding children
 
@@ -123,10 +129,6 @@ function displayComment(obj) {
   commentTxtWrapper.appendChild(usercomment);
   comment.appendChild(image);
   comment.appendChild(commentTxtWrapper);
-  // commentSection.appendChild(comment);
   commentSection.appendChild(comment);
   commentSection.appendChild(hr);
-}
-for (i = 0; i < commentArr.length; i++) {
-  displayComment(commentArr[i]);
 }
